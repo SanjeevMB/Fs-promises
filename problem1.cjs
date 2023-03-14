e95581030094470a2ab2b247719f62eff29c1805
= <=>
2023-03-14 15:13:10 +0530

    Question 1 completed

---
+++ problem1.cjs
@@ -0,0 +1,130 @@
+/*
+    Problem 1:
+    
+    Using callbacks and the fs module's asynchronous functions, do the following:
+        1. Create a directory of random JSON files
+        2. Delete those files simultaneously 
+*/
+
+
+const { rejects } = require('assert');
+const { resolveCname } = require('dns/promises');
+const fs = require('fs');
+const { resolve } = require('path');
+const path = require('path');
+
+function createDirectoryAndDeleteTheDirectoryFiles() {
+
+    let directoryName = './createdDirectory';
+    let fileRefrenceArray = new Array(10);
+    fileRefrenceArray.fill(0);
+
+    function creatingDirectory(directoryName) {
+
+        return new Promise((resolve, rejects) => {
+
+            fs.mkdir(path.join(__dirname, directoryName), (error) => {
+
+                if (error) {
+
+                    rejects(error);
+
+                } else {
+
+                    resolve('directory created');
+
+                }
+
+            });
+
+        });
+
+    }
+
+    let createdDirectory = creatingDirectory(directoryName);
+
+    createdDirectory.then((data) => {
+
+        console.log(data.toString());
+
+    });
+
+    function filesCreation(fileRefrenceArray) {
+
+        return new Promise((resolve, rejects) => {
+
+            return fileRefrenceArray.map((elment, index, array) => {
+
+                fs.writeFile(
+
+                    `${path.join(__dirname, `${directoryName}`,
+                        `file${index + 1}.json`)}`,
+
+                    JSON.stringify({ 'name': 'Sanjeev', 'roll_no': 100 }),
+
+                    (error) => {
+
+                        if (error) {
+
+                            rejects(error);
+
+                        } else {
+
+                            resolve('files created');
+
+                        }
+
+                    });
+
+            });
+
+        });
+
+    }
+
+    let fileCreated = filesCreation(fileRefrenceArray);
+
+    fileCreated.then((message) => {
+
+        console.log(message);
+
+    });
+
+    function fileDeletion(fileName) {
+
+        return new Promise((resolve, rejects) => {
+
+            fs.unlink(fileName, (error) => {
+
+                    if (error) {
+
+                        rejects(error);
+
+                    } else {
+
+                        resolve('files deleted');
+
+                    }
+
+                });
+
+        });
+
+    }
+
+    let message = fileRefrenceArray.map((nums, index) => {
+
+        let fileDeteted = fileDeletion(`${path.join(__dirname, `${directoryName}`,
+        `file${index + 1}.json`)}`);
+
+        fileDeteted.then((data) => {
+
+            console.log(data.toString());
+
+        });
+
+    });
+
+}
+
+module.exports = createDirectoryAndDeleteTheDirectoryFiles;
---
+++ test/testProblem1.cjs
@@ -0,0 +1,3 @@
+const createDirectoryAndDeleteTheDirectoryFiles = require('../problem1.cjs');
+
+createDirectoryAndDeleteTheDirectoryFiles();
